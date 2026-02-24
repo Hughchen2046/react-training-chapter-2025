4decc0e8cd3796cd906aa7e6a32c457c09cae4c5
Hugh <hughchen2046@gmail.com>
2026-02-24 13:01:46 +0800

    updated

--- week7/src/features/loading/Loading.jsx
+++ week7/src/features/loading/Loading.jsx
@@ -4,15 +4,15 @@ import { Slab } from 'react-loading-indicators';
 
 export function GlobalLoading() {
   const isGlobalLoading = useSelector(stateIsLoading('global'));
-
   if (!isGlobalLoading) return null;
   return (
-    <Slab
-      color={['#32cd32', '#327fcd', '#cd32cd', '#cd8032']}
-      size="large"
-      speedPlus="-3"
-      text=""
-      textColor=""
-    />
+    <>
+      <div
+        className="position-fixed top-0 start-0 w-100 h-100 d-flex justify-content-center align-items-center bg-dark bg-opacity-50"
+        style={{ zIndex: 9999 }}
+      >
+        <Slab color="#cc31ac" size="large" text="loading..." textColor="#810b0b" />
+      </div>
+    </>
   );
 }
